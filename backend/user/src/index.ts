0380dd5d808af7782a54d10c0305f1d84ea3b731
rahulharsh017 <rahulharsh017@gmail.com>
2026-06-22 02:41:59 +0530

    Created User Model and did connection for Redis

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,14 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 
 connectDb();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to redis"))
+  .catch(console.error);
+
 const app = express();
 
 const port = process.env.PORT;
 
-app.listen(port,() => {
-    console.log(`User service is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`User service is running on port ${port}`);
 });
